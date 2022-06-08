9f61a86e38b63bea13f06f5d936b939b114bfbd2
Netlight <netlight@Netlights-MacBook-Pro.local>
2022-06-09 02:04:26 +0300

    Add iOS implementation for getPlatformAndVersion method

--- ios/TestModule.m
+++ ios/TestModule.m
@@ -2,8 +2,7 @@
 
 @interface RCT_EXTERN_MODULE(TestModule, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(getPlatformAndVersion:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
 
 @end
--- ios/TestModule.swift
+++ ios/TestModule.swift
@@ -1,8 +1,18 @@
+import Foundation;
+import UIKit;
+
+struct TestClass {
+    let platform: String
+    let version: String
+}
+
 @objc(TestModule)
 class TestModule: NSObject {
+    
+    var info = TestClass(platform: "iOS", version: UIDevice.current.systemVersion)
 
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
+    @objc(getPlatformAndVersion:withRejecter:)
+    func getPlatformAndVersion(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(String(format: "Platform: %@, Version: %@", info.platform, info.version))
     }
 }
