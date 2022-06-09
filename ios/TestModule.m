d716ac4daaf80348a27b9c04f8c5ef9e66418d94
Netlight <netlight@Netlights-MacBook-Pro.local>
2022-06-09 03:12:54 +0300

    Add simple-event implementation for iOS

--- ios/TestModule.m
+++ ios/TestModule.m
@@ -1,6 +1,9 @@
 #import <React/RCTBridgeModule.h>
+#import "React/RCTEventEmitter.h"
 
-@interface RCT_EXTERN_MODULE(TestModule, NSObject)
+@interface RCT_EXTERN_MODULE(TestModule, RCTEventEmitter)
+
+RCT_EXTERN_METHOD(dispatchSimpleEvent)
 
 RCT_EXTERN_METHOD(getPlatformAndVersion:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject)
--- ios/TestModule.swift
+++ ios/TestModule.swift
@@ -7,12 +7,21 @@ struct TestClass {
 }
 
 @objc(TestModule)
-class TestModule: NSObject {
+class TestModule: RCTEventEmitter {
     
-    var info = TestClass(platform: "iOS", version: UIDevice.current.systemVersion)
+    private var _info = TestClass(platform: "iOS", version: UIDevice.current.systemVersion)
+    
+    @objc
+    func dispatchSimpleEvent() {
+      sendEvent(withName: "simple-event", body: ["info": String(format: "Platform: %@, Version: %@", _info.platform, _info.version)])
+    }
+
+    override func supportedEvents() -> [String]! {
+      return ["simple-event"]
+    }
 
     @objc(getPlatformAndVersion:withRejecter:)
     func getPlatformAndVersion(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(String(format: "Platform: %@, Version: %@", info.platform, info.version))
+        resolve(String(format: "Platform: %@, Version: %@", _info.platform, _info.version))
     }
 }
